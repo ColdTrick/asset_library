ac45a6f74518f21ceb7e370be59edfb95cd9e87b
Jeroen Dalsem <jdalsem@coldtrick.com>
2016-07-22 13:31:08 +0200

    chore: added dutch translation

---
+++ languages/nl.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * This file was created by Translation Editor v5.1
+ * On 2016-07-22 13:30
+ */
+
+return array (
+  'asset_library' => 'Asset Library',
+  'admin:administer_utilities:assets' => 'Beheer assets',
+  'asset_library:admin:info' => 'Upload bestanden die publiekelijk beschikbaar zijn om te worden gebruikt op de site. Kopieer de links van de bestanden om overal te (her)gebruiken',
+  'asset_library:admin:assets:list' => 'Bestanden',
+  'asset_library:forms:upload:file:label' => 'Selecteer een bestand om te uploaden',
+);
